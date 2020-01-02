29a7c8fcb9ac1d6e779b8ca2c16964294e4530ef
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-01-03 00:10:36 +0530

    removed some extra tests from headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -154,152 +154,121 @@ describe('extractFirstNLines', function () {
 });
 
 describe('head', function () {
-
-  context('when file is given', function () {
-    let stream, streamPicker;
-    beforeEach(function () {
-      stream = { setEncoding: fake(), on: fake(), destroy: fake() };
-      streamPicker = {};
-    });
-
-    it('should give 10 lines of file for default case ', function (done) {
-      const userOptions = ['one.txt'];
-
-      streamPicker.pick = function(filePath){
-        assert.strictEqual(filePath, 'one.txt');
-        return stream;
-      };
-
-      const displayResult = (result) => {
-        assert.strictEqual(result.output, '1\n2\n3\n4\n5\n6\n7\n8\n9\n10');
-        assert.strictEqual(result.error, '');
-        done();
-      };
-      
-      head(userOptions, streamPicker, displayResult);
-      assert(stream.setEncoding.calledWith('utf8'));
-      assert(stream.on.firstCall.calledWith('data'));
-      assert(stream.on.thirdCall.calledWith('end'));
-      stream.on.firstCall.lastArg('1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11');
-      stream.on.thirdCall.lastArg();
-    });
-
-    it('should give all lines when file has lines < 10', function (done) {
-      const userOptions = ['one.txt'];
-
-      streamPicker.pick = function (filePath) {
-        assert.strictEqual(filePath, 'one.txt');
-        return stream;
-      };
-
-      const displayResult = (result) => {
-        assert.strictEqual(result.output, 'abc');
-        assert.strictEqual(result.error, '');
-        done();
-      };
-
-      head(userOptions, streamPicker, displayResult);
-      assert(stream.setEncoding.calledWith('utf8'));
-      assert(stream.on.firstCall.calledWith('data'));
-      assert(stream.on.thirdCall.calledWith('end'));
-      stream.on.firstCall.lastArg('abc');
-      stream.on.thirdCall.lastArg();
-    });
-
-    it('should give error when wrong file is present', function (done) {
-      const userOptions = ['badFile.txt'];
-
-      streamPicker.pick = function (filePath) {
-        assert.strictEqual(filePath, 'badFile.txt');
-        return stream;
-      };
-
-      const err = { path: 'badFile.txt' };
-      const errMsg = `head: ${err.path}: No such file or directory`;
-      const displayResult = (result) => {
-        assert.strictEqual(result.output, '');
-        assert.strictEqual(result.error, errMsg);
-        done();
-      };
-
-      head(userOptions, streamPicker, displayResult);
-      assert(stream.setEncoding.calledWith('utf8'));
-      assert(stream.on.secondCall.calledWith('error'));
-      stream.on.secondCall.lastArg(err);
-    });
-
-    it('should give error when line count is less than 1', function (done) {
-      const count = 0;
-      const userOptions = ['-n', count, 'one.txt'];
-
-      streamPicker.pick = function (filePath) {
-        assert.strictEqual(filePath, 'one.txt');
-        return stream;
-      };
-
-      const err = { path: 'one.txt' };
-      const displayResult = (result) => {
-        assert.strictEqual(result.output, '');
-        assert.strictEqual(result.error, 'head: illegal line count -- 0');
-        done();
-      };
-
-      head(userOptions, streamPicker, displayResult);
-      assert(stream.setEncoding.calledWith('utf8'));
-      assert(stream.on.secondCall.calledWith('error'));
-      stream.on.secondCall.lastArg(err);
-    });
-
-    it('should give error when line count is not integer', function (done) {
-      const userOptions = ['-n', 'm', 'one.txt'];
-
-      const createReadStream = function (fileName) {
-        assert.strictEqual(fileName, 'badFile.txt');
-        return stream;
-      };
-
-      const reader = { createReadStream, stdin: {} };
-      const err = { path: 'one.txt' };
-      const displayResult = (result) => {
-        assert.strictEqual(result.output, '');
-        assert.strictEqual(result.error, 'head: illegal line count -- m');
-        done();
-      };
-
-      head(userOptions, reader, displayResult);
-      assert(stream.setEncoding.calledWith('utf8'));
-      assert(stream.on.secondCall.calledWith('error'));
-      stream.on.secondCall.lastArg(err);
-    });
+  let stream, streamPicker;
+  beforeEach(function () {
+    stream = { setEncoding: fake(), on: fake(), destroy: fake() };
+    streamPicker = {};
   });
 
-  context('when file is not given', function () {
-    let stdin;
-    beforeEach(function () {
-      stdin = { setEncoding: fake(), on: fake(), destroy: fake() };
-    });
+  it('should give 10 lines when line count is not given', function (done) {
+    const userOptions = ['one.txt'];
 
-    it('should load the 10 lines from stdin for default case', function (done) {
-      const userOptions = [''];
-      const streamPicker = {};
+    streamPicker.pick = function(filePath){
+      assert.strictEqual(filePath, 'one.txt');
+      return stream;
+    };
 
-      streamPicker.pick = function (filePath) {
-        assert.strictEqual(filePath, '');
-        return stdin;
-      };
+    const displayResult = (result) => {
+      assert.strictEqual(result.output, '1\n2\n3\n4\n5\n6\n7\n8\n9\n10');
+      assert.strictEqual(result.error, '');
+      done();
+    };
+    
+    head(userOptions, streamPicker, displayResult);
+    assert(stream.setEncoding.calledWith('utf8'));
+    assert(stream.on.firstCall.calledWith('data'));
+    assert(stream.on.thirdCall.calledWith('end'));
+    stream.on.firstCall.lastArg('1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11');
+    stream.on.thirdCall.lastArg();
+  });
 
-      const displayResult = (result) => {
-        assert.strictEqual(result.output, '1\n2\n3\n4\n5\n6\n7\n8\n9\n10');
-        assert.strictEqual(result.error, '');
-        done();
-      };
+  it('should give specified lines when line count is given', function (done) {
+    const userOptions = ['one.txt'];
 
-      head(userOptions, streamPicker, displayResult);
-      assert(stdin.setEncoding.calledWith('utf8'));
-      assert(stdin.on.firstCall.calledWith('data'));
-      assert(stdin.on.thirdCall.calledWith('end'));
-      stdin.on.firstCall.lastArg('1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11');
-      stdin.on.thirdCall.lastArg();
-    });
+    streamPicker.pick = function (filePath) {
+      assert.strictEqual(filePath, 'one.txt');
+      return stream;
+    };
+
+    const displayResult = (result) => {
+      assert.strictEqual(result.output, 'abc');
+      assert.strictEqual(result.error, '');
+      done();
+    };
+
+    head(userOptions, streamPicker, displayResult);
+    assert(stream.setEncoding.calledWith('utf8'));
+    assert(stream.on.firstCall.calledWith('data'));
+    assert(stream.on.thirdCall.calledWith('end'));
+    stream.on.firstCall.lastArg('abc');
+    stream.on.thirdCall.lastArg();
+  });
+
+  it('should give error when wrong file is present', function (done) {
+    const userOptions = ['badFile.txt'];
+
+    streamPicker.pick = function (filePath) {
+      assert.strictEqual(filePath, 'badFile.txt');
+      return stream;
+    };
+
+    const err = { path: 'badFile.txt' };
+    const errMsg = `head: ${err.path}: No such file or directory`;
+    const displayResult = (result) => {
+      assert.strictEqual(result.output, '');
+      assert.strictEqual(result.error, errMsg);
+      done();
+    };
+
+    head(userOptions, streamPicker, displayResult);
+    assert(stream.setEncoding.calledWith('utf8'));
+    assert(stream.on.secondCall.calledWith('error'));
+    stream.on.secondCall.lastArg(err);
+  });
+
+  it('should give error when line count is less than 1', function (done) {
+    const count = 0;
+    const userOptions = ['-n', count, 'one.txt'];
+
+    streamPicker.pick = function (filePath) {
+      assert.strictEqual(filePath, 'one.txt');
+      return stream;
+    };
+
+    const err = { path: 'one.txt' };
+    const displayResult = (result) => {
+      assert.strictEqual(result.output, '');
+      assert.strictEqual(result.error, 'head: illegal line count -- 0');
+      done();
+    };
+
+    head(userOptions, streamPicker, displayResult);
+    assert(stream.setEncoding.calledWith('utf8'));
+    assert(stream.on.secondCall.calledWith('error'));
+    stream.on.secondCall.lastArg(err);
+  });
+
+  it('should give error when line count is not integer', function (done) {
+    const userOptions = ['-n', 'm', 'one.txt'];
+
+    const createReadStream = function (fileName) {
+      assert.strictEqual(fileName, 'badFile.txt');
+      return stream;
+    };
+
+    const reader = { createReadStream, stdin: {} };
+    const err = { path: 'one.txt' };
+    const displayResult = (result) => {
+      assert.strictEqual(result.output, '');
+      assert.strictEqual(result.error, 'head: illegal line count -- m');
+      done();
+    };
+
+    head(userOptions, reader, displayResult);
+    assert(stream.setEncoding.calledWith('utf8'));
+    assert(stream.on.secondCall.calledWith('error'));
+    stream.on.secondCall.lastArg(err);
   });
 });
+
+
